598612c49185a225eb83cc29dd93824f681a94ad
so0601pk <youhami9@gmail.com>
2021-11-16 07:47:09 +0900

    Calc,App.vueを作成

--- calc_app/src/App.vue
+++ calc_app/src/App.vue
@@ -1,15 +1,52 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="id">
+    <Calc v-bind:title="message" v-on:result-event="appAction" />
+    <hr>
+    <div>
+      <table v-html="log"></table>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/Calc.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Calc
+  },
+  data: function(){
+    return{
+      message:'CALC',
+      result:[]
+    }
+  },
+  computed:{
+    log:function(){
+      var table = '<tr><th class="head">Expression</th><th class="head">Value</th></tr>';
+      for(var i in this.result){
+        table += '<tr><td>' + this.result[i][0] + '</td></tr>' + this.result[i][1] + '</th></tr>';
+      }
+      return table;
+    }
+  },
+  created: function(){
+    var items = localStorage.getTtem('log');
+    var logs = JSON.parse(items);
+    if(logs != null){
+      this.result = logs;
+    }
+  },
+  methods:{
+    appAction: function(exp, res){
+      this.result.unshift([exp, res]);
+      if(this.result.length > 10){
+        this.result.pop();
+      }
+      var log = JSON.stringify(this.result);
+      localStorage.setItem('log', log);
+    }
   }
 }
 </script>
@@ -19,8 +56,23 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  text-align: left;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 5px;
+}
+
+tr td{
+  padding: 5px;
+  border: 1px solid gray;
+}
+
+tr th{
+  padding: 5px;
+  border: 1px solid gray;
+}
+
+tr th.head{
+  background-color:black;
+  color:white;
 }
 </style>
---
+++ calc_app/src/components/Calc.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="hello">
+    <h1>{{ title }}</h1>
+    <p>{{ message }}</p>
+    <hr>
+    <div>
+      <div>
+        <textarea v-model="fomula" cols="40" rows="5"></textarea>
+      </div>
+      <div>
+        <button v-on:click="doAction">CALC</button>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Calc',
+  props: {
+    title: String
+  },
+  data:function(){
+    return{
+      message: 'Enter expression:',
+      fomula:'0',
+    };
+  },
+  methods:{
+    doActoin: function(){
+      var arr = this.fomula.trim().split('¥n');//両端の空白を削除し、改行で分割
+      var last = arr.pop();
+      var fn = '';
+      for(var n in arr ){
+        if(arr[n].trum() != ''){
+          fn += 'var' + arr[n] + ';';
+        }
+      }
+      fn += 'return' + 'last' + ';';
+      var exp = 'function f(){ '+ fn +' } f();';
+      var ans = eval(exp);
+      this.message = 'answer:' + ans;
+      var re = arr.join(';').trim();
+      if(re != ''){
+        re += ';'
+      }
+      re += last;
+      this.$emit('result-event', re, ans);
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
