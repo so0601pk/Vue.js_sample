3cac7da879bb17e881ca536c77f9803c5ad20df3
so0601pk <youhami9@gmail.com>
2021-11-17 07:40:25 +0900

    vueのporpsのバリデーションを作成

--- hello_app/src/App.vue
+++ hello_app/src/App.vue
@@ -15,7 +15,7 @@ export default {
   data: function(){
     return {
       message: 'validation',
-      num:123
+      num:12
     };
   },
   methods:{
--- hello_app/src/components/HelloWorld.vue
+++ hello_app/src/components/HelloWorld.vue
@@ -12,7 +12,13 @@ export default {
   name: 'HelloWorld',
   props: {
     title: String,
-    num: Number,
+    num:{
+      type: Number,
+      default: 100,
+      validator :function(value){
+        return value == parseInt(value) && value >= 0 && value <= 100;
+      },
+    },
   },
   data:function(){//data=変数だが、関数の形式で記述しなければならない
     return{
